dc4e25535a68f450884480c63734bd1192214f9c
kmnsoo <wkdska22@naver.com>
2021-09-06 09:49:48 +0900

    Data 전송, 받기 기능 추가

--- DetailScreen.js
+++ DetailScreen.js
@@ -1,14 +1,18 @@
 // DetailScreen.js
-import React, { Component } from 'react';
-import { View, Text, } from 'react-native'; 
+import React, { Component } from 'react'; 
+import { View, Text, } from 'react-native';
 
 export default class DetailScreen extends Component { 
-    render() { 
+    render() {
         return ( 
         <View> 
-            <Text style={{fontSize:30}}>Detail Screen!!</Text> 
-        </View> 
-        ); 
-    }
-}
+            <Text style={{fontSize:30}}>Detail Screen</Text>
+             <Text>Paramater Value</Text>
+              <Text style={{fontSize:30, backgroundColor: '#aaaaaa', borderRadius: 5, padding:5, margin:5}}>
+                   {this.props.route.params.textInputValue}
+                   </Text>
+                    </View> 
+                   ); 
+                } 
+            }
 
--- MainScreen.js
+++ MainScreen.js
@@ -1,20 +1,35 @@
 //MainScreen.js
-import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+
+import React, { Component } from 'react'; 
+import { View, Text, Button, TextInput } from 'react-native'; 
 
 export default class MainScreen extends Component { 
+    
+    state={ 
+        textInputValue:null
+     } 
+     state = {
+        count: 0
+      }
+    
+      onPress = () => {
+        this.setState({
+          count: this.state.count + 1
+        })
+      }
     render() { 
         return ( 
         <View>
-             <Text style={{fontSize:30}}>Main Screen!!</Text>
-              <Button onPress={() => this.goMainScreen()} title='Detail Screen 으로 이동'/> 
-              </View> 
-              ); 
-            }
+             <Text style={{fontSize:30}}>Main Screen</Text>
+              <TextInput style={{ backgroundColor: '#aaaaaa', borderRadius: 5, padding:5, margin:5 }} 
+              onChangeText={(value) => this.setState({textInputValue: value})}/> 
+              <Button onPress={() => this.goMainScreen()} title='Go Detail Screen'/> 
+              </View>
+               );
+            } 
             
-    goMainScreen(){ 
-        // DetailScreen으로 화면 이동 
-        this.props.navigation.navigate('DETAIL');
-     } 
-    }
+    goMainScreen(){
+         this.props.navigation.navigate('DETAIL', {textInputValue:this.state.textInputValue}); 
+        }
+     }
 
